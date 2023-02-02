d024a7acd9370e1dc3e3c999d9f1d4ba195f8c36
andy <ashiraki@anduril.com>
2023-02-02 00:00:53 -0800

    wk5lab1

---
+++ Labs/week5/lab7_exploration.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 22:53:45 2023
+
+@author: ashiraki
+"""
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+file = 'https://raw.githubusercontent.com/grbruns/cst383/master/College.csv'
+
+df = pd.read_csv(file)
